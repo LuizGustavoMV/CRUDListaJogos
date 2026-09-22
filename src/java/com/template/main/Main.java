aeb7b3b0f6e8989f716c24f78c2705e66c944bc5
ra2357102 <luiz.martins-valerio@unesp.br>
2026-09-22 09:32:30 -0300

    Luiz

--- src/java/com/template/main/Main.java
+++ src/java/com/template/main/Main.java
@@ -9,7 +9,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader(Main.class.getResource("/com/template/main.fxml"));
-        Scene scene = new Scene(loader.load(), 600, 400);
+        Scene scene = new Scene(loader.load());
         stage.setTitle("Lista Jogos");
         stage.setScene(scene);
         stage.show();
